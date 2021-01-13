745351286e88ba647f686b59a31e360a28d7a2a0
Donzellini <muriel.donzellini@gmail.com>
2021-01-13 20:33:36 -0300

    adding comments to tupla.py

--- Aula_04/tuplas.py
+++ Aula_04/tuplas.py
@@ -2,6 +2,7 @@
 
 tuple = ( 'abcd', 786 , 2.23, 'john', 70.2  )
 tinytuple = (123, 'john')
+supertupla = tuple + tinytuple
 
 print(tuple)               # Prints the complete tuple
 print(tuple[0])            # Prints first element of the tuple
@@ -9,7 +10,8 @@ print(tuple[1:3])          # Prints elements of the tuple starting from 2nd till
 print(tuple[2:])           # Prints elements of the tuple starting from 3rd element
 print(tinytuple * 2)       # Prints the contents of the tuple twice
 print(tuple + tinytuple)   # Prints concatenated tuples
+print(supertupla)          # mostrando a tupla em uma função dentro da variável
 
-tuple[0] = "hahaha"
+# tuple[0] = "hahaha" # mostra que a tupla não suporta mudança de variáveis dentro dessa "array"
 
 #a tupla é uma espécie de constante
