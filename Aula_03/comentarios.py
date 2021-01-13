5890dd56ef5444e8a6abbba6e6f1b5b8fa27f740
Donzellini <muriel.donzellini@gmail.com>
2021-01-12 21:30:55 -0300

    adding exercise comentarios.py

---
+++ Aula_03/comentarios.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# First comment
+print("Hello, Python!") # second comment
+
+
+
+# This is a comment.
+# This is a comment, too.
+# This is a comment, too.
+# I said that already.
+
+
+'''
+This is a multiline
+comment.
+'''
+
+print("Hello, Python!") # second comment
