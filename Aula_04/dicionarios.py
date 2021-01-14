ff89547211b6d083b20b636f94617c963204cee2
Donzellini <muriel.donzellini@gmail.com>
2021-01-13 21:00:38 -0300

    adding exercise dicionarios.py

---
+++ Aula_04/dicionarios.py
@@ -0,0 +1,20 @@
+dict = {}
+dict['one'] = "This is one"
+dict[2]     = "This is two"
+
+tinydict = {'name': 'john', 'code':6734, 'dept': 'sales'}
+
+print(dict['one'])
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
+
+dict['one'] = "This is not one"
+
+print(dict['one'])
+parametros = {'ligar': True, 'posicao':'6734', 'timeout': 3000}
+print(parametros)
+
+#o dicionário é utilizado principalmente com arquivos .json
+#com ele podemos criar parâmetros
