279345ef63e890213b76e049ceef8d54d985b734
Donzellini <muriel.donzellini@gmail.com>
2021-01-16 10:15:16 -0300

    adding lists4.py and list5.py

---
+++ Aula_10/listas4.py
@@ -0,0 +1,9 @@
+lista_comida = ["arroz", "feijão", "carne", "batata"]
+
+#ler de trás para frente -> usa o negativo
+print(lista_comida[-2])
+
+#ler de trás para frente usando o range
+print(lista_comida[-2:])
+print(lista_comida[:-2])
+print(lista_comida[1:-2])
