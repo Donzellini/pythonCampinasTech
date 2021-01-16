5ca76b002d267801fbd31a8d3ade0a9cc03c2448
Donzellini <muriel.donzellini@gmail.com>
2021-01-16 11:18:09 -0300

    adding a kind of search in tuples

--- Aula_11/tuplas5.py
+++ Aula_11/tuplas5.py
@@ -1,8 +1,12 @@
 tup1 = (1,2,3)
 tup2 = (4,5,6)
-#retorna a quantidade de elementos
-print(len(tup1))
-#retorna a concatenação das tuplas
-print(tup1 + tup2)
-#retorna a repetição dos elementos da tupla
-print(tup1 * 3)
+# #retorna a quantidade de elementos
+# print(len(tup1))
+# #retorna a concatenação das tuplas
+# print(tup1 + tup2)
+# #retorna a repetição dos elementos da tupla
+# print(tup1 * 3)
+
+#fazer uma busca
+if 3 in tup1:
+    print("3 está em tup1")
