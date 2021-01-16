56c1b2ad6cd26e667db266ed1469ede727277c27
Donzellini <muriel.donzellini@gmail.com>
2021-01-16 13:54:15 -0300

    updating files dict2.py and dict3.py. dict3.py must be revised. adding dict4.py

--- Aula_12/dict2.py
+++ Aula_12/dict2.py
@@ -1,8 +1,8 @@
-dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
-dict['Age'] = 8; # update existing entry
-dict['School'] = "DPS School"; # Add new entry
-dict['Chavequeeuqueiraadicionar'] = "Valorqueeuqueiraadicionar"
+# dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+# dict['Age'] = 8; # update existing entry
+# dict['School'] = "DPS School"; # Add new entry
+# dict['Chavequeeuqueiraadicionar'] = "Valorqueeuqueiraadicionar"
 
-print("dict['Age']: ", dict['Age'])
-print("dict['School']: ", dict['School'])
-print(dict)
+# print("dict['Age']: ", dict['Age'])
+# print("dict['School']: ", dict['School'])
+# print(dict)
--- Aula_12/dict3.py
+++ Aula_12/dict3.py
@@ -1,11 +1,21 @@
-dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
-dict['Age'] = 8; # update existing entry
-dict['School'] = "DPS School"; # Add new entry
-#Adicionando um Chave Valor ao dicionário
-dict['Chavequeeuqueiraadicionar'] = "Valorqueeuqueiraadicionar"
-#Alterando um Valor de uma chave existente
-dict['Class'] = 'Second'
+# dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+# del dict['Name'] # remove entry with key 'Name'
+# dict.clear()     # remove all entries in dict
+# del dict         # delete entire dictionary
 
-print("dict['Age']: ", dict['Age'])
-print("dict['School']: ", dict['School'])
-print(dict)
+# print("dict['Age']: ", dict['Age'])
+# print("dict['School']: ", dict['School'])
+
+dict_compras = {"Nome":"Secador de cabelo","Preço":"R$ 1000,00", "Tamanho":"Médio"}
+
+print(dict_compras)
+# #Excluí todos os elementos mas não excluí o dicionario da memória
+# dict_compras.clear()
+# print(dict_compras)
+# #Exclui o dicionário da memória
+# del dict_compras
+# print(dict_compras)
+# #Excluí um elemento do dicionário
+del dict_compras["Nome"]
+print(dict_compras)
+
