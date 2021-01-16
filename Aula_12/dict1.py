9faee41f68f715cf2d94d62e9f78f94f5cffd4df
Donzellini <muriel.donzellini@gmail.com>
2021-01-16 13:32:10 -0300

    adding folder to 12th class

---
+++ Aula_12/dict1.py
@@ -0,0 +1,8 @@
+dict = {'Name': 'Zara'
+       , 'Age': 7
+     , 'Class': 'First'}
+print("dict['Name']: ", dict['Name'])
+print("dict['Age']: ", dict['Age'])
+print("dict['Class']: ", dict['Class'])
+print("dict: ", dict)
+
