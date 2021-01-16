403aed6191a923fd8ae102bd90e83073c7a6c429
Donzellini <muriel.donzellini@gmail.com>
2021-01-16 11:22:54 -0300

    adding for structure to show how to navigate in tuples

--- Aula_11/tuplas5.py
+++ Aula_11/tuplas5.py
@@ -1,5 +1,6 @@
 tup1 = (1,2,3)
 tup2 = (4,5,6)
+tup_cervejas = ('brahma', 'skol', 'malzibier', 'amstel', 'heineken')
 # #retorna a quantidade de elementos
 # print(len(tup1))
 # #retorna a concatenação das tuplas
@@ -10,3 +11,7 @@ tup2 = (4,5,6)
 #fazer uma busca
 if 3 in tup1:
     print("3 está em tup1")
+
+#varrer (ou navegar) através dos itens da tupla
+for item in tup_cervejas:
+    print(item)
