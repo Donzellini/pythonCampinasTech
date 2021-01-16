fc23263846b2f2dfe7450683dae15567051603df
Donzellini <muriel.donzellini@gmail.com>
2021-01-16 11:03:35 -0300

    adding folder to 11th class and files tuplas1.py and tuplas2.py

---
+++ Aula_11/tuplas1.py
@@ -0,0 +1,16 @@
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5 )
+tup3 = "a", "b", "c", "d"
+
+print(tup3)
+
+tup4 = ()
+print(tup4)
+tup4 = (76,)
+print(tup4)
+tup4 = (76,88,)
+print(tup4)
+
+
+# tup4[0] = 77 #não suporta a atribuição de itens
+# print(tup4)
