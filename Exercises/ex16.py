331cd4464108a8a5ead4c425e617a4781a9a0dd8
Donzellini <muriel.donzellini@gmail.com>
2021-01-17 18:46:50 -0300

    adding title to exercise 16

---
+++ Exercises/ex16.py
@@ -0,0 +1,2 @@
+# 16. Fazer um sistema de Agenda de contatos (Deve criar um dicionário com Nome, Telefone e Endereço, Imprimir os dados do dicionário, ser capaz de inserir e excluir um contato)
+# source: https://academiahopper.com.br/dicionarios-em-python/
