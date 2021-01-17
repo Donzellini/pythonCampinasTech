c2bfb3917a85b3b5179b860ad103188c093d2f34
Donzellini <muriel.donzellini@gmail.com>
2021-01-17 11:31:22 -0300

    adding solved exercise 6

---
+++ Exercises/ex6.py
@@ -0,0 +1,5 @@
+#6. Algoritmo para criar uma lista usando range com 100 elementos e imprimi-la
+
+#conta de 0 a 99, totalizando 100 elementos
+for numero in range(100):
+    print(f"Número  {numero}")
