e73f4e383307e61b12c8ec11f929fff5493ad283
Donzellini <muriel.donzellini@gmail.com>
2021-01-17 10:56:02 -0300

    adding exercise 2

---
+++ Exercises/ex2.py
@@ -0,0 +1,12 @@
+# 2. Algoritmo para ir ao banco para sacar dinheiro (Imprimir a sequência para ir ao banco e sacar dinheiro)
+
+print("01. Chegar ao banco")
+print("02. Entrar na fila")
+print("03. Ao chegar sua vez, dirigir-se ao caixa eletrônico")
+print("04. Inserir seu cartão")
+print("05. Verificar seu saldo em conta")
+print("06. Escolher a opçao de saque")
+print("07. Digitar o valor desejado")
+print("08. Esperar a contagem do caixa eletrônico")
+print("09. Retirar o dinheiro")
+print("10. Sair da agência")
