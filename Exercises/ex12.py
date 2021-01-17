2fd9506fa73c79d7a736e367535bfecfd280f1e2
Donzellini <muriel.donzellini@gmail.com>
2021-01-17 18:11:29 -0300

    adding exercise 12 solved

---
+++ Exercises/ex12.py
@@ -0,0 +1,10 @@
+# 12. Elabore um algoritmo em Python que:
+# a) Primeiro exiba uma mensagem de boas vindas;
+# b) Pergunte o nome do usuário;
+# c) Exiba uma mensagem dizendo uma mensagem de olá
+# seguida pelo nome do usuário seguida por outra mensagem
+# fazendo um elogio.
+
+print("SEJA MUITO BEM-VINDO/BEM-VINDA!")
+nome = input("Qual é seu nome?: ")
+print(f"Olá, {nome}! Você é bem perspicaz!")
