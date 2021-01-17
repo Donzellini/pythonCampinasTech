e0e89283f436e0c878aae567c5bf42c2c0a2d5cb
Donzellini <muriel.donzellini@gmail.com>
2021-01-17 11:25:10 -0300

    adding solved exercise 5

--- Exercises/ex5.py
+++ Exercises/ex5.py
@@ -1,1 +1,12 @@
 #5. Algoritmo para calcular a média do aluno (Deve colocar o nome do aluno, colocar 4 notas e imprimir sua média)
+
+print("MÉDIA DO ALUNO")
+print("________________")
+print("")
+nome = (input("Digite o nome do aluno: "))
+n1 = int(input("Digite a 1ª nota: "))
+n2 = int(input("Digite a 2ª nota: "))
+n3 = int(input("Digite a 3ª nota: "))
+n4 = int(input("Digite a 4ª nota: "))
+media = (n1 + n2 + n3 + n4)/4
+print(f"A média de {nome} é: {media}")
