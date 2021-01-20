4ca7544a01d2de42588b4b80774f390e36782e61
Donzellini <muriel.donzellini@gmail.com>
2021-01-20 01:09:56 -0300

    testting some functions and trying to solve ex18.py

--- Aula_04/dicionarios.py
+++ Aula_04/dicionarios.py
@@ -1,20 +1,22 @@
 dict = {}
 dict['one'] = "This is one"
 dict[2]     = "This is two"
+print(dict)
 
 tinydict = {'name': 'john', 'code':6734, 'dept': 'sales'}
+# print(tinydict.keys())
 
-print(dict['one'])
-print(dict[2])
-print(tinydict)
-print(tinydict.keys())
-print(tinydict.values())
+# print(dict['one'])
+# print(dict[2])
+# print(tinydict)
+# print(tinydict.keys())
+# print(tinydict.values())
 
-dict['one'] = "This is not one"
+# dict['one'] = "This is not one"
 
-print(dict['one'])
+# print(dict['one'])
 parametros = {'ligar': True, 'posicao':'6734', 'timeout': 3000}
 print(parametros)
 
-#o dicionário é utilizado principalmente com arquivos .json
-#com ele podemos criar parâmetros
+# #o dicionário é utilizado principalmente com arquivos .json
+# #com ele podemos criar parâmetros
--- Exercises/ex18.py
+++ Exercises/ex18.py
@@ -1,32 +1,71 @@
-#source: https://academiahopper.com.br/dicionarios-em-python/
+print("----------------------------")
+print("       FEIRA DA FRUTA")
+print("----------------------------")
+#variáveis globais
+lista_opcoes = ["Ver Produtos", "Comprar", "Sair"]
+lista_compra = []
+dict1 = {'produto': 'morango', 'preço':"R$4,50"}
+dict2 = {'produto': 'abacaxi', 'preço':"R$6,00"}
+dict3 = {'produto': 'lichia', 'preço':"R$10,00"}
+opcao = 0
+print(dict1)
 
-command = "continue"
-contacts = {}
+# funções
+def listagem():
+    i = 0
+    t = len(lista_opcoes) - 1
+    while i <= t:
+        for num in lista_opcoes:
+            print(f"[{i}] {num}")
+            i += 1
 
-def procura_chaves(dicionario, nome):
-    chaves = []
-    for chave in dicionario:
-        if chave.startswith(nome):
-            chaves.append(chave)
-    return chaves
+# def sair():
+#     print("Saindo!")
 
-while command != "sair":
-    command = input("Escolha uma opção: (novo, pes, sair):")
+# def opcao_invalida():
+#     print("Digite uma opção válida.")
 
-    if command == "novo":
-        nome = input("Nome: ").strip()
-        telefone = input("Telefone: ").strip()
-        email = input("E-mail: ").strip()
-        contacts[nome] = {
-            "nome": nome,
-            "telefone": telefone,
-            "email": email
-        }
+def produtos():
+    c1 = 1
+    c2 = 2
+    c3 = 3
+    print(f"[{c1}] {dict1}")
+    print(f"[{c2}] {dict2}")
+    print(f"[{c3}] {dict3}")
+    print("")
 
-    if command == "pes":
-        nome = input("Nome: ").lower()
-        chaves_encontradas = procura_chaves(contacts, nome)
+# def comprar():    
+#     produto1 = (item1[1])
+#     preco1 = (item1[2])
+#     produto2 = (item1[1])
+#     preco2 = (item1[2])
+#     produto3 = (item1[1])
+#     preco3 = (item1[2])
+#     lista_compra.append([produto, preco])
 
-        if len(chaves_encontradas) > 0:
-            for chave in chaves_encontradas:
-                print(contacts[chave])
+# def visualizar_agenda():
+#     indice = 0
+#     for item in lista_compra:
+#         print(f"{indice} - {item}")
+#         indice += 1
+
+listagem()
+
+#leitor do input e trigger para as funções
+while opcao != 3:
+    opcao = int(input("OPÇÃO: "))
+
+    if opcao == 0:
+        produtos()
+        listagem()
+
+    if opcao == 1:
+        visualizar_agenda()
+
+    if opcao == 2:
+        sair()
+        break
+else:
+    opcao_invalida()
+    listagem()
+    
