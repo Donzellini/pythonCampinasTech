9160061cf5dcd8252137612eafac212c9201c693
Donzellini <muriel.donzellini@gmail.com>
2021-01-20 22:13:15 -0300

    adding solved exercise 18. using native function sum to show the user the total purchase

--- Aula_04/dicionarios.py
+++ Aula_04/dicionarios.py
@@ -1,7 +1,7 @@
 dict = {}
 dict['one'] = "This is one"
 dict[2]     = "This is two"
-print(dict)
+# print(dict)
 
 tinydict = {'name': 'john', 'code':6734, 'dept': 'sales'}
 # print(tinydict.keys())
@@ -10,13 +10,13 @@ tinydict = {'name': 'john', 'code':6734, 'dept': 'sales'}
 # print(dict[2])
 # print(tinydict)
 # print(tinydict.keys())
-# print(tinydict.values())
+print(tinydict.values())
 
 # dict['one'] = "This is not one"
 
-# print(dict['one'])
-parametros = {'ligar': True, 'posicao':'6734', 'timeout': 3000}
-print(parametros)
+# # print(dict['one'])
+# parametros = {'ligar': True, 'posicao':'6734', 'timeout': 3000}
+# print(parametros)
 
 # #o dicionário é utilizado principalmente com arquivos .json
 # #com ele podemos criar parâmetros
--- Exercises/ex18.py
+++ Exercises/ex18.py
@@ -2,15 +2,17 @@ print("----------------------------")
 print("       FEIRA DA FRUTA")
 print("----------------------------")
 #variáveis globais
-lista_opcoes = ["Ver Produtos", "Comprar", "Sair"]
+lista_opcoes = ["Ver Produtos", "Comprar", "Total", "Sair"]
 lista_compra = []
-dict1 = {'produto': 'morango', 'preço':"R$4,50"}
-dict2 = {'produto': 'abacaxi', 'preço':"R$6,00"}
-dict3 = {'produto': 'lichia', 'preço':"R$10,00"}
+total_compra = []
+dict1 = [{'item': 0, 'produto': 'morango', 'preco': 4.50}, 
+         {'item': 1, 'produto': 'abacaxi', 'preco': 6.00}, 
+         {'item': 2,'produto': 'lichia', 'preco': 10.00}]
 opcao = 0
-print(dict1)
+print(dict1[0]['produto'])
 
 # funções
+# lista as opções do menu
 def listagem():
     i = 0
     t = len(lista_opcoes) - 1
@@ -19,35 +21,32 @@ def listagem():
             print(f"[{i}] {num}")
             i += 1
 
-# def sair():
-#     print("Saindo!")
+#opção sair
+def sair():
+    print("Saindo!")
 
-# def opcao_invalida():
-#     print("Digite uma opção válida.")
+#opção inválida
+def opcao_invalida():
+    print("Digite uma opção válida.")
 
+#lista os produtos cadastrados no dicionário que pode virar um .json
 def produtos():
-    c1 = 1
-    c2 = 2
-    c3 = 3
-    print(f"[{c1}] {dict1}")
-    print(f"[{c2}] {dict2}")
-    print(f"[{c3}] {dict3}")
-    print("")
+    print(dict1)
 
-# def comprar():    
-#     produto1 = (item1[1])
-#     preco1 = (item1[2])
-#     produto2 = (item1[1])
-#     preco2 = (item1[2])
-#     produto3 = (item1[1])
-#     preco3 = (item1[2])
-#     lista_compra.append([produto, preco])
+#menu de compras
+#sugestão: deseja continuar comprado? (S/N)
+def comprar():
+    print(dict1)
+    item = int(input("Escolha um dos produtos acima: "))
+    if item == dict1[item]['item']:
+        lista_compra.append([dict1[item]])
+        total_compra.append([dict1[item]['preco']])
+    print("Sua lista de compras está abaixo: ")
+    print(lista_compra)
 
-# def visualizar_agenda():
-#     indice = 0
-#     for item in lista_compra:
-#         print(f"{indice} - {item}")
-#         indice += 1
+#soma os valores numéricos da lista total_compras, gerada na def comprar()
+def total():
+    print(f'O total da sua compra é: {sum([ item[0] for item in total_compra])}')
 
 listagem()
 
@@ -60,9 +59,14 @@ while opcao != 3:
         listagem()
 
     if opcao == 1:
-        visualizar_agenda()
-
+        comprar()
+        listagem()
+    
     if opcao == 2:
+        total()
+        listagem()
+
+    if opcao == 3:
         sair()
         break
 else:
