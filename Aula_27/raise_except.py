30c9a20303605fe195b50a3bc29dfef1f5ba2cd8
Donzellini <muriel.donzellini@gmail.com>
2021-01-30 14:46:39 -0300

    raise exception

---
+++ Aula_27/raise_except.py
@@ -0,0 +1,12 @@
+def functionName( level ):
+    if level <1:
+        raise Exception(level)
+    #the code below to this would not be exdcuted if we raise the exception
+    return level
+
+
+try:
+    l = functionName(-10)
+    print ("level = ", 1)
+except Exception as e:
+    print("fofinho, deu erro... olha o valor",e.args[0])
